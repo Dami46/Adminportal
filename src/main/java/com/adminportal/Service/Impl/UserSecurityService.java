0dce5a8536212467316831e50cd78b166b2d41e8
Dami46 <damian.urbaniak.work@gmail.com>
2021-09-12 20:48:51 +0200

    Admin Security

--- src/main/java/com/adminportal/Service/Impl/UserSecurityService.java
+++ src/main/java/com/adminportal/Service/Impl/UserSecurityService.java
@@ -2,6 +2,7 @@ package com.adminportal.Service.Impl;
 
 import com.adminportal.Domain.User;
 import com.adminportal.Repository.UserRepository;
+import com.adminportal.Security.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,11 @@ public class UserSecurityService implements UserDetailsService {
         User user = userRepository.findByUsername(username);
         if (null == user) {
             throw new UsernameNotFoundException("Username not found");
+        }
+        for (UserRole role : user.getUserRoles()) {
+            if (role.getRole().getRoleId() != 0) {
+                throw new UsernameNotFoundException("You have no authorization");
+            }
         }
         return user;
     }
